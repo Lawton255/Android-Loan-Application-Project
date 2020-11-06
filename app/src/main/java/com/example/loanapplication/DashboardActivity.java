47c773bd8f4ba771ec336a3d16eb8ff6a4905d33
Lawton255 <elawton255@gmail.com>
2020-11-06 07:57:07 +0300

    Firebasestore integrated

--- app/src/main/java/com/example/loanapplication/DashboardActivity.java
+++ app/src/main/java/com/example/loanapplication/DashboardActivity.java
@@ -6,13 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class DashboardActivity extends AppCompatActivity {
-    Button mBtnRequestLoan, mBtnPayLoan, mBtnLoanHistory, mBtnLoanCalculator;
+    Button mBtnRequestLoan, mBtnPayLoan, mBtnLoanHistory, mBtnLoanCalculator, mBtnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,7 @@ public class DashboardActivity extends AppCompatActivity {
         mBtnPayLoan = findViewById(R.id.btn_pay_loan);
         mBtnLoanHistory = findViewById(R.id.btn_history);
         mBtnLoanCalculator = findViewById(R.id.btn_loan_calc);
+        mBtnProfile = findViewById(R.id.btn_profile);
 
         mBtnRequestLoan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +52,13 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), LoanCalculator.class));
+            }
+        });
+
+        mBtnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/loanapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/loanapplication/RegistrationActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +16,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class RegistrationActivity extends AppCompatActivity {
     EditText mEdtSname, mEdtEmail, mEdtPhone, mEdtPassword;
@@ -24,6 +33,8 @@ public class RegistrationActivity extends AppCompatActivity {
     TextView mTxtLogin;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,7 @@ public class RegistrationActivity extends AppCompatActivity {
         mTxtLogin = findViewById(R.id.txt_view_log);
         progressBar = findViewById(R.id.regprogressBar);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
 
         if (fAuth.getCurrentUser() != null){
@@ -49,22 +61,29 @@ public class RegistrationActivity extends AppCompatActivity {
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullName = mEdtSname.getText().toString().trim();
-                String email = mEdtEmail.getText().toString().trim();
-                String phone = mEdtPhone.getText().toString().trim();
+                final String fullName = mEdtSname.getText().toString();
+                final String email = mEdtEmail.getText().toString().trim();
+                final String phone = mEdtPhone.getText().toString();
                 String password = mEdtPassword.getText().toString().trim();
 
 
+                if (TextUtils.isEmpty(fullName)){
+                    mEdtEmail.setError("Enter Full name.");
+                    return;
+                }
+                if (TextUtils.isEmpty(email)){
+                    mEdtEmail.setError("Email is required.");
+                    return;
+                }
+                if (phone.length() < 10){
+                    mEdtEmail.setError("Phone number must be greater than 10 numbers");
+                    return;
+                }
+                if (password.length() < 6){
+                    mEdtEmail.setError("Password must be >= to 6 characters");
+                    return;
+                }
 
-                if (fullName.isEmpty()){
-                    mEdtSname.setError("Enter Full name.");
-                }else if (email.isEmpty()){
-                    mEdtEmail.setError("Enter email.");
-                } else if (phone.length() < 10){
-                    mEdtPhone.setError("Phone number must be greater than 10 numbers");
-                }else if (password.length() < 6){
-                    mEdtPassword.setError("Password must be >= to 6 characters");
-                }else {
 
                    progressBar.setVisibility(View.VISIBLE);
 
@@ -74,6 +93,18 @@ public class RegistrationActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()){
                                 Toast.makeText(RegistrationActivity.this, "User successful created.", Toast.LENGTH_SHORT).show();
+                                userID = fAuth.getCurrentUser().getUid(); // call the userID from stored data
+                                DocumentReference documentReference = fStore.collection("users").document(userID);
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("fName", fullName);
+                                user.put("email", email);
+                                user.put("phone", phone);
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("TAG", "onsuccess: user profile is created for" + userID);
+                                    }
+                                });
                                 startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
                             }else {
                                 Toast.makeText(RegistrationActivity.this, "Error occured !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -81,7 +112,6 @@ public class RegistrationActivity extends AppCompatActivity {
 
                         }
                     });
-                }
 
             }
         });
