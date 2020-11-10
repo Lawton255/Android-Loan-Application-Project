ee0b3342f8dff67a732b0f49c35bc13dc2d67035
Lawton255 <elawton255@gmail.com>
2020-11-10 23:43:01 +0300

    Change / Update password implemented to user profile

--- app/src/main/java/com/example/loanapplication/ProfileActivity.java
+++ app/src/main/java/com/example/loanapplication/ProfileActivity.java
@@ -1,15 +1,23 @@
 package com.example.loanapplication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -18,9 +26,10 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class ProfileActivity extends AppCompatActivity {
     TextView fullname, email, phone;
-    Button mBtnLogout;
+    Button mBtnLogout, mBtnChangePassword;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    FirebaseUser user;
     String userID;
 
 
@@ -33,11 +42,13 @@ public class ProfileActivity extends AppCompatActivity {
         email = findViewById(R.id.profile_email);
         phone = findViewById(R.id.profile_phone);
         mBtnLogout = findViewById(R.id.btn_logout);
+        mBtnChangePassword = findViewById(R.id.btn_change_password);
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
 
         userID = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
 
         DocumentReference documentReference = fStore.collection("users").document(userID);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
@@ -47,6 +58,46 @@ public class ProfileActivity extends AppCompatActivity {
                 phone.setText(documentSnapshot.getString("phone"));
                 fullname.setText(documentSnapshot.getString("fName"));
                 email.setText(documentSnapshot.getString("email"));
+            }
+        });
+
+        mBtnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetPassword = new EditText(v.getContext());
+
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Change Password ?");
+                passwordResetDialog.setMessage("Enter new password > Characters long. ");
+                passwordResetDialog.setView(resetPassword);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Change password
+                        String newPassword = resetPassword.getText().toString();
+                        user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(ProfileActivity.this, "Password changed successfully ", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(ProfileActivity.this, "Password change Failed!!"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close the dialog
+                    }
+                });
+                passwordResetDialog.create().show();
             }
         });
 
